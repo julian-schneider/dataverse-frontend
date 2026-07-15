8120831d4365a78ccdbed43d12a5d9e7f12aad29
Cheng Shi <cks5520psu@gmail.com>
2026-07-15 11:17:37 -0400

    improve test coverage

--- tests/component/sections/session/SessionProvider.spec.tsx
+++ tests/component/sections/session/SessionProvider.spec.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, useLocation } from 'react-router-dom'
 import { AuthContext } from 'react-oauth2-code-pkce'
 import { ReadError } from '@iqss/dataverse-client-javascript'
 import { UserRepository } from '@/users/domain/repositories/UserRepository'
@@ -35,6 +35,12 @@ describe('SessionProvider', () => {
     )
   }
 
+  const LocationDisplay = () => {
+    const location = useLocation()
+
+    return <p data-testid="location">{`${location.pathname}${location.search}`}</p>
+  }
+
   const renderComponent = ({
     loginInProgress,
     withTokenPresent
@@ -59,7 +65,16 @@ describe('SessionProvider', () => {
           <Routes>
             <Route element={<SessionProvider />}>
               <Route index element={<ComponentUsingContext />} />
-              <Route path="sign-up" element={<div>Sign up</div>} />
+              <Route
+                path="sign-up"
+                element={
+                  <>
+                    <ComponentUsingContext />
+                    <LocationDisplay />
+                    <div>Sign up</div>
+                  </>
+                }
+              />{' '}
             </Route>
           </Routes>
         </WithRepositories>
@@ -169,6 +184,8 @@ describe('SessionProvider', () => {
     })
 
     cy.findByText('Sign up').should('exist')
+    cy.findByText(BEARER_TOKEN_IS_VALID_BUT_NOT_LINKED_MESSAGE).should('exist')
+    cy.findByTestId('location').should('have.text', '/sign-up?validTokenButNotLinkedAccount=true')
   })
 
   it('should detect any other ReadError instances', () => {
